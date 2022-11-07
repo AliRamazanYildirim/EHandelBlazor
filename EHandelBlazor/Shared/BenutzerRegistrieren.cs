8f7bf949fc30787fdc82a38a098ab52e08fb0192
aliramazanyildirim <aliramazanyildirim@gmail.com>
2022-11-07 15:40:36 +0300

    Benutzer-Register Modelle erstellen

---
+++ EHandelBlazor/Shared/BenutzerRegistrieren.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EHandelBlazor.Shared
+{
+    public class BenutzerRegistrieren
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Passwort { get; set; } = string.Empty;
+        public string PasswortBestätigen { get; set; } = string.Empty;
+
+    }
+}
