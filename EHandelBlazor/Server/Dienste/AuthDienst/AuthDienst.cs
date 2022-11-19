1b0df445c7029a5aa72adfbe53c118c2e5777c40
aliramazanyildirim <aliramazanyildirim@gmail.com>
2022-11-19 08:04:13 +0300

    Implementieren Sie die Benutzerregistrierung auf dem Server ->AuthDienst

--- EHandelBlazor/Server/Dienste/AuthDienst/AuthDienst.cs
+++ EHandelBlazor/Server/Dienste/AuthDienst/AuthDienst.cs
@@ -1,4 +1,6 @@
-﻿namespace EHandelBlazor.Server.Dienste.AuthDienst
+﻿using System.Security.Cryptography;
+
+namespace EHandelBlazor.Server.Dienste.AuthDienst
 {
     public class AuthDienst : IAuthDienst
     {
@@ -17,10 +19,36 @@
             }
             return false;
         }
-
-        public Task<DienstAntwort<int>> RegistrierenAsync(Benutzer benutzer, string passwort)
+        private void ErstellenPasswortHash(string passwort, out byte[] passwortHash, out byte[] passwortSalz) 
         {
-            throw new NotImplementedException();
+            using(var hashHersteller=new HMACSHA512())
+            {
+                passwortSalz = hashHersteller.Key;
+                passwortHash = hashHersteller.ComputeHash(System.Text.Encoding.UTF8.GetBytes(passwort));
+            }
+        }
+
+        public async Task<DienstAntwort<int>> RegistrierenAsync(Benutzer benutzer, string passwort)
+        {
+            if(await BenutzerExistiertAsync(benutzer.Email))
+            {
+                return new DienstAntwort<int> 
+                {
+                    Erfolg = false, 
+                    Nachricht = "Benutzer existiert bereits." 
+                };
+            }
+            ErstellenPasswortHash(passwort, out byte[] passwortHash, out byte[] passwortSalz);
+            benutzer.PasswortHash = passwortHash;
+            benutzer.PasswortSalz = passwortSalz;
+
+            _kontext.Benutzer.Add(benutzer);
+            await _kontext.SaveChangesAsync();
+
+            return new DienstAntwort<int>
+            {
+                Daten = benutzer.ID
+            };
         }
     }
 }
