ccb7c0faebf02ee1fafe94e69184bf9eb2435c9f
aliramazanyildirim <aliramazanyildirim@gmail.com>
2022-11-19 08:21:02 +0300

    Addieren AuthController ->AuthController

---
+++ EHandelBlazor/Server/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EHandelBlazor.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthDienst _authDienst;
+
+        public AuthController(IAuthDienst authDienst)
+        {
+            _authDienst = authDienst;
+        }
+        [HttpPost("registrierung")]
+        public async Task<ActionResult<DienstAntwort<int>>> Registrieren(BenutzerRegistrieren anfrage)
+        {
+            var antwort = await _authDienst.RegistrierenAsync(
+            new Benutzer
+            {
+                Email = anfrage.Email
+            },
+            anfrage.Passwort);
+
+            if(!antwort.Erfolg)
+            {
+                return BadRequest(antwort);
+            }
+            return Ok(antwort);
+        }
+    }
+}
