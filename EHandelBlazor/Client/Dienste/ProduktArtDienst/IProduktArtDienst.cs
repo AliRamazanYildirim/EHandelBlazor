eb6c8d8ef6f0b8b622d7d95d0c2f945afb71df0c
aliramazanyildirim <aliramazanyildirim@gmail.com>
2023-03-28 14:36:26 +0200

    Produktarten auf dem Client hinzufügen und aktualisieren

--- EHandelBlazor/Client/Dienste/ProduktArtDienst/IProduktArtDienst.cs
+++ EHandelBlazor/Client/Dienste/ProduktArtDienst/IProduktArtDienst.cs
@@ -5,5 +5,9 @@
         event Action BeiÄnderung;
         public List<ProduktArt> ProduktArten { get; set; }
         Task GeheZurAlleProduktArten();
+        Task ErstelleProduktArt(ProduktArt produktArt);
+        Task AktualisiereProduktArt(ProduktArt produktArt);
+        ProduktArt ErstelleNeueProduktArt();
+
     }
 }
--- EHandelBlazor/Client/Dienste/ProduktArtDienst/ProduktArtDienst.cs
+++ EHandelBlazor/Client/Dienste/ProduktArtDienst/ProduktArtDienst.cs
@@ -12,6 +12,28 @@
 
         public event Action BeiÄnderung;
 
+        public async Task AktualisiereProduktArt(ProduktArt produktArt)
+        {
+            var antwort = await _httpClient.PutAsJsonAsync("api/produktart", produktArt);
+            ProduktArten = (await antwort.Content.ReadFromJsonAsync<DienstAntwort<List<ProduktArt>>>()).Daten;
+            BeiÄnderung.Invoke(); ;
+        }
+
+        public ProduktArt ErstelleNeueProduktArt()
+        {
+            var neueProduktArt = new ProduktArt { IstNeu = true, Bearbeitung = true };
+            ProduktArten.Add(neueProduktArt);
+            BeiÄnderung.Invoke();
+            return neueProduktArt;
+        }
+
+        public async Task ErstelleProduktArt(ProduktArt produktArt)
+        {
+            var antwort = await _httpClient.PostAsJsonAsync("api/produktart", produktArt);
+            ProduktArten = (await antwort.Content.ReadFromJsonAsync<DienstAntwort<List<ProduktArt>>>()).Daten;
+            BeiÄnderung.Invoke();
+        }
+
         public async Task GeheZurAlleProduktArten()
         {
             var resultat = await _httpClient.GetFromJsonAsync<DienstAntwort<List<ProduktArt>>>("api/produktart");
